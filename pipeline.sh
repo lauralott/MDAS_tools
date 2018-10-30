4cf458b20695a0c02adc7716a31ef4cfcf690d14
paulopez78 <pau.lopez.78@gmail.com>
2018-10-31 00:27:30 +0100

    pipeline bash script

---
+++ pipeline.sh
@@ -0,0 +1,57 @@
+votingappPath='./src/votingapp'
+
+build(){
+  pushd $votingappPath
+  ./deps.sh
+  rm -rf ./deploy
+  (go build -o ./deploy/votingapp && cp -r ui ./deploy) || return 1
+  popd
+}
+
+run(){
+  app='votingapp'
+  pushd $votingappPath
+  pid=$(ps | grep $app | awk '{ print $1 }' | head -1)
+  kill -9 $pid || true
+  ./deploy/$app &
+  popd
+}
+
+test(){
+  http_client(){
+    curl --url 'http://localhost:8080/vote' \
+      --request $1 \
+      --data "$2" \
+      --header 'Content-Type: application/json' \
+      --silent
+  }
+
+  topics='{"topics":["bash","python","go"]}' 
+  expectedWinner='bash'
+  echo "Given voting topics $topics, When vote for $options, Then winner is $expectedWinner"
+
+  http_client POST $topics
+  for option in bash bash bash python
+  do
+    http_client PUT '{"topic":"'$option'"}'
+  done
+  winner=$(http_client DELETE | jq -r '.winner')
+
+  if [ "$expectedWinner" = "$winner" ]; then
+    return 0
+  else 
+    return 1
+  fi
+}
+
+if build > log 2> error; then
+  echo "Build Completed"
+  run
+  if test; then
+    echo "Test Passed"
+  else
+    echo "Test Failed"
+  fi
+else
+  echo "FAILED"
+fi
