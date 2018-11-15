caf87832160881ee9a697f835c6a4bd03eda9729
Laura <lauraelena0804@gmail.com>
2018-11-15 23:57:12 +0100

    S3: homework bonus

--- test/test.py
+++ test/test.py
@@ -1,25 +1,47 @@
 import requests
+import time
 import sys
 
-topics=["bash","python","go"]
-URL ='http://votingapp:8080/vote'
-headers = {'Content-type': 'application/json'}
-expectedWinner = 'bash'
-votes = ["bash","bash","bash","python"]
-#print("Given voting topics " + str(topics) + ", when vote for " + str(votes) + ", then winner is " + expectedWinner)
+def BashWins():
+    topics=["bash","python","go"]
+    URL ='http://votingapp:8080/vote'
+    headers = {'Content-type': 'application/json'}
+    expectedWinner = 'bash'
+    votes = ["bash","bash","bash","python"]
+    print("Given voting topics " + str(topics) + ", when vote for " + str(votes) + ", then winner is " + expectedWinner)
 
-r = requests.post(url=URL, json={'topics':topics}, headers=headers)
-#print("Post status: " + str(r.status_code))
+    r = requests.post(url=URL, json={'topics':topics}, headers=headers)
+    print("Post status: " + str(r.status_code))
 
-for i in range(len(votes)) :
-    r = requests.put(url=URL, json={'topic':votes[i]}, headers=headers)
+    for i in range(len(votes)) :
+        r = requests.put(url=URL, json={'topic':votes[i]}, headers=headers)
 
-r = requests.delete(url=URL).json()
-winner = r['winner']
+    r = requests.delete(url=URL).json()
+    winner = r['winner']
 
-if (winner == expectedWinner):
-    print("Test OK")
-    sys.exit()
-else:
-    print("test FAILED")
-    sys.exit(1)
+    if (winner == expectedWinner):
+        print("Test OK")
+        sys.exit()
+    else:
+        print("test FAILED")
+        sys.exit(1)
+
+
+def Retry(func):
+    waitingSeconds = 1
+    print("Init test (py)...")
+    for i in range(0,3):
+        print("Try " + str(i))
+        try:
+            func()   
+        except requests.exceptions.RequestException as e:
+            print(e)
+            if i==2:
+                sys.exit(1)
+            time.sleep(waitingSeconds)
+            waitingSeconds = waitingSeconds * 5
+            continue
+        break
+
+Retry(BashWins)
+
--- test/test.sh
+++ test/test.sh
@@ -1,26 +1,50 @@
 url='http://votingapp:8080/vote'
-http_client(){
-    curl --url url \
-    --request $1 \
-    --data  "$2" \
-    --header 'Content-Type: application/json'
+bash_wins(){
+    set -e 
+    http_client(){
+        curl --url url \
+        --request $1 \
+        --data  "$2" \
+        --header 'Content-Type: application/json' \
+        --silent \
+        --output /dev/null  \
+        --write-out "%{http_code}"
+    }
+    topics='{"topics":["bash","python","go"]}'
+    expectedWinner='bash'
+    echo "Given voting topics $topics, when vote for $options, then winner is $expectedWinner"   
+
+    status=$(http_client POST $topics)
+    if [  "$status" -ne 200 ]; then
+        echo "status $status"
+        return 1
+    fi    
+
+    for option in bash bash bash python
+    do 
+        http_client PUT '{"topic":"'$option'"}'
+    done
+
+    winner=$(http_client DELETE | jq -r '.winner')
+
+    if [ "$expectedWinner" = "$winner" ]; then
+        echo "Test OK"
+        exit 0
+    else
+        echo "Test Failed"
+        exit 1
+    fi
+
 }
-topics='{"topics":["bash","python","go"]}'
-expectedWinner='bash'
-echo "Given voting topics $topics, when vote for $options, then winner is $expectedWinner"   
 
-http_client POST $topics
-
-for option in bash bash bash python
-do 
-    http_client PUT '{"topic":"'$option'"}'
-done
-
-winner=$(http_client DELETE | jq -r '.winner')
-
-if [ "$expectedWinner" = "$winner" ]; then
-    exit 0
-else
-    echo "Test Failed"
+retry(){
+    waitingSeconds=2
+    echo "Init test (sh)..."
+    for i in 1 2 3; 
+    do 
+        echo "Try $i" && $1 && break || sleep $waitingSeconds;
+    done
     exit 1
-fi
+}
+
+retry bash_wins
